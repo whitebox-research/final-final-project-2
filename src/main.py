e5cadec8e745b94509ad9429a884cc0d70cad2d9
Clark Urzo <git@zrkrlc.com>
2024-01-19 16:32:00 +0800

    add more stuff

--- src/main.py
+++ src/main.py
@@ -1,7 +1,41 @@
+class Calc:
+    def __init__(self, ex):
+        self.ex = ex.replace(' ', '')
+        self.i = 0
+
+    def pse(self):
+        try:
+            return self.n()
+        except Exception as e:
+            return f"invalid: {e}"
+
+    def n(self):
+        sgn = False
+
+        if self.ex[self.i] == '-':
+            sgn = True
+            self.i += 1
+            
+        if self.ex[self.i] == '(':
+            self.i += 1
+            out = None # TODO
+            self.i += 1
+            return -out if sgn else out
+        
+        i_0 = self.i
+        while self.i < len(self.ex) and self.ex[self.i].isdigit():
+            self.i += 1
+
+        n = float(self.ex[i_0:self.i])
+        return -n if sgn else n
+
+
 if __name__ == '__main__':
     while True:
         try:
             eq = input("> ")
-            print(eq)
+            calc = Calc(eq)
+            out = calc.pse()
+            print(out)
         except KeyboardInterrupt:
             exit()
